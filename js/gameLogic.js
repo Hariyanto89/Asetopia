72a8e565aeddd47e14ce392d1378d01933f06048
Hariyanto <yantonft@gmail.com>
2024-11-23 17:43:03 +0700

    Update gameLogic.js
    Signed-off-by: Hariyanto <yantonft@gmail.com>

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -7,7 +7,9 @@ function mineRuby(location) {
     location.resource -= 10; // Kurangi sumber daya lokasi
     ruby += 10; // Tambahkan ruby
     updateRubyUI(); // Perbarui tampilan ruby
+    checkCompletion(); // Periksa apakah semua lokasi mining selesai
     playSound("mining.mp3"); // Putar suara mining
+    console.log(`Berhasil mining di ${location.name}. Ruby: ${ruby}`);
   } else {
     alert(`Sumber daya di ${location.name} habis!`);
   }
@@ -20,13 +22,41 @@ function upgradeAsset() {
     profit += 20; // Tambahkan profit
     updateProfitUI(); // Perbarui tampilan profit
     playSound("upgrade.mp3"); // Putar suara upgrade
+    alert(`Upgrade berhasil! Profit meningkat menjadi ${profit}.`);
   } else {
     alert("Ruby tidak cukup untuk upgrade aset!");
   }
 }
 
-// Fungsi memainkan efek suara
+// Fungsi untuk memberikan reward setelah quest selesai
+function giveReward() {
+  const rewardAmount = 100; // Jumlah ruby sebagai reward
+  ruby += rewardAmount;
+  updateRubyUI();
+  alert(`Quest selesai! Anda mendapatkan ${rewardAmount} ruby.`);
+}
+
+// Fungsi untuk memeriksa apakah semua lokasi mining selesai
+function checkCompletion() {
+  if (merigiLocations.every((loc) => loc.resource === 0)) {
+    completeQuest(); // Tandai quest sebagai selesai
+    giveReward(); // Berikan reward
+    alert("Selamat! Semua lokasi di Kecamatan Merigi selesai. Lanjutkan ke map berikutnya.");
+  }
+}
+
+// Fungsi untuk memainkan efek suara
 function playSound(file) {
   const sound = new Audio(`assets/sounds/${file}`);
-  sound.play().catch(err => console.error("Gagal memutar efek suara:", err));
+  sound.play().catch((err) => console.error("Gagal memutar efek suara:", err));
+}
+
+// Fungsi untuk reset game (opsional untuk testing)
+function resetGame() {
+  ruby = 0;
+  profit = 0;
+  merigiLocations.forEach((loc) => (loc.resource = 100)); // Reset resource di semua lokasi
+  updateRubyUI();
+  updateProfitUI();
+  alert("Game telah di-reset.");
 }
