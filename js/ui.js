30eaadd22bb41d96e0a600526d4ac4d1c3841a39
Hariyanto <yantonft@gmail.com>
2024-11-23 16:41:10 +0700

    Update ui.js
    Signed-off-by: Hariyanto <yantonft@gmail.com>

--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,20 @@
+// Fungsi memperbarui tampilan ruby
 function updateRubyUI() {
   document.getElementById("ruby").textContent = ruby;
 }
 
+// Fungsi memperbarui tampilan profit
 function updateProfitUI() {
   document.getElementById("profit").textContent = profit;
 }
 
-// Fungsi inisialisasi UI
+// Fungsi inisialisasi game
 function initializeGame() {
   updateRubyUI();
   updateProfitUI();
+
+  // Tombol upgrade aset
+  document.getElementById("upgradeButton").addEventListener("click", () => {
+    upgradeAsset();
+  });
 }
