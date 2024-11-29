03df85e149cdc9678ac5a0f7a9a8baa7935014b3
Hariyanto <yantonft@gmail.com>
2024-11-29 19:55:39 +0700

    Update gameplay.js
    Signed-off-by: Hariyanto <yantonft@gmail.com>

--- js/gameplay.js
+++ js/gameplay.js
@@ -335,7 +335,7 @@ function initializeKecamatanData() {
 
 // Fungsi inisialisasi map
 function initializeMap(kecamatanData) {
-    const map = L.map("kepahiangMap").setView([-3.6403, 102.6159], 12);
+    const map = L.map("kepahiangMap").setView([-3.504305, 102.517364], 12);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "© OpenStreetMap contributors",
